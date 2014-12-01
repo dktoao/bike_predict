dd22c2318174b56ef735fba84695e32ee52a8909
Daniel Kuntz <daniel.kuntz@dk-cloud.net>
2014-12-01 09:21:57 -0700

    Minor Changes

--- sandbox.py
+++ sandbox.py
@@ -27,9 +27,12 @@ for set_name in ['train.csv', 'test.csv']:
 training_data = data[0]
 test_data = data[1]
 
+# Descriptive variables
+desc_var = ['hour']
+
 # Get data of interest
-X = training_data[['year', 'month', 'hour', 'workingday', 'weather', 'windspeed']].as_matrix()
-X_p = test_data[['year', 'month', 'hour', 'workingday', 'weather', 'windspeed']].as_matrix()
+X = training_data[desc_var].as_matrix()
+X_p = test_data[desc_var].as_matrix()
 X_w = test_data[['year', 'month', 'day', 'hour']].as_matrix()
 #X = X.reshape(-1, 1)
 y = training_data['count'].as_matrix()
@@ -37,8 +40,8 @@ y = training_data['count'].as_matrix()
 #print(X_p)
 
 # Evaluate SVM with a one input variable
-out = evaluate_predictor(X, y, support_vector_regression, n_trials=3, C=1, epsilon=1)
-print('Estimated Error:\n\tMean: {0:.3e}\n\tVar: {1:.3e}'.format(mean(out), var(out)))
+#out = evaluate_predictor(X, y, support_vector_regression, n_trials=3, C=10, epsilon=0.1)
+#print('Estimated Error:\n\tMean: {0:.3e}\n\tVar: {1:.3e}'.format(mean(out), var(out)))
 
 # Linear Regression
 #model = LinearRegression(fit_intercept=False)
@@ -51,9 +54,13 @@ print('Estimated Error:\n\tMean: {0:.3e}\n\tVar: {1:.3e}'.format(mean(out), var(
 #lin_out = model.predict(X_p)
 #write_data(X_w, lin_out, 'out_svr2')
 
-'''
+
 # Plot data
-hourly_data = data.groupby('hour').mean()
+X_new = arange(0, 23).reshape(-1, 1)
+model = SVR(C=10)
+model.fit(X, y)
+lin_out = model.predict(X_new)
+hourly_data = training_data.groupby('hour').mean()
 plt.figure('Average Bike Use By Hour')
 plt.title('Average Bike Use By Hour')
 plt.plot(hourly_data.index, hourly_data['count'], 'b', label='Actual Use')
@@ -65,4 +72,3 @@ plt.legend(loc=0)
 plt.xlabel('Time of Day (hour)')
 plt.ylabel('Average Number of Bikes Rented')
 plt.show()
-'''
